aa7349f146b13aa0108abfe1b66a45b76ec8a06c
Nazhim <nazhimkalam@gmail.com>
2021-01-24 00:10:42 +0530

    added the product main page

--- src/pages/GemsPages/Gem/Gem.js
+++ src/pages/GemsPages/Gem/Gem.js
@@ -1,11 +1,16 @@
-import './Gem.css'
+import { useHistory } from 'react-router-dom';
+import './Gem.css';
 
-function Gem() {
-    return (
-        <div>
-            
-        </div>
-    )
+function Gem({ img, name, viewMoreUrl }) {
+	const history = useHistory();
+
+	return (
+		<div className="gem">
+			<img src={img} alt="" />
+			<p>{name}</p>
+			<button onClick={() => history.push(viewMoreUrl)}>VIEW MORE</button>
+		</div>
+	);
 }
 
-export default Gem
+export default Gem;
--- src/pages/GemsPages/GemStonesMain/GemStonesMain.js
+++ src/pages/GemsPages/GemStonesMain/GemStonesMain.js
@@ -1,9 +1,9 @@
+import Gem from '../Gem/Gem';
 import './GemStonesMain.css';
 
 function GemStonesMain() {
 	return (
 		<div className="gemStonesMain">
-
 			<div className="gemStonesMain__details">
 				<div className="gemStonesMain__detailsFirst">
 					<h1>HIGH QUALITY SAPPHIRE STONES</h1>
@@ -15,21 +15,37 @@ function GemStonesMain() {
 						We are the leading suppliers of the highest quality rough and cut-and-polished gem stones for both locally
 						and internationally
 					</p>
-                    <div className="gemStonesMain__detailsFirstButtons">
-                        <button>VIEW PRODUCTS</button>
-                        <button>CALL US</button>
-                    </div>
+					<div className="gemStonesMain__detailsFirstButtons">
+						<button>VIEW PRODUCTS</button>
+						<button>CALL US</button>
+					</div>
 				</div>
 				<div className="gemStonesMain__detailsSecond">
-                    <img src="https://images-aka.kay.com/kay/gemstone/blue-gems/blue-stone-names.jpg" alt=""/>
-                </div>
+					<img src="https://images-aka.kay.com/kay/gemstone/blue-gems/blue-stone-names.jpg" alt="" />
+				</div>
 			</div>
 
 			<div className="gemStonesMain__otherGems">
-				<div className="gemStonesMain__otherGemsRowOne"></div>
-				<div className="gemStonesMain__otherGemsRowOne"></div>
-				<div className="gemStonesMain__otherGemsRowOne"></div>
-				<div className="gemStonesMain__otherGemsRowOne"></div>
+				<div className="gemStonesMain__otherGemsRow">
+					<Gem img="gems/teal-sapphire.png" name="Teal Sapphires" viewMoreUrl="/teal-sapphire" />
+					<Gem img="gems/purple-sapphire.png" name="Purple Sapphires" viewMoreUrl="/purple-sapphire" />
+				</div>
+				<div className="gemStonesMain__otherGemsRow">
+					<Gem
+						img="gems/Padparadscha-sapphire.png"
+						name="Padparadscha Sapphires"
+						viewMoreUrl="/padparadscha-sapphire"
+					/>
+					<Gem img="gems/orange-sapphire.png" name="Orange Sapphires" viewMoreUrl="/orange-sapphire" />
+				</div>
+				<div className="gemStonesMain__otherGemsRow">
+					<Gem
+						img="gems/ClaudiaHamann_PinkSapphire_Ceylon_Cushion_Unheated.png"
+						name="Pink Sapphires"
+						viewMoreUrl="/pink-sapphire"
+					/>
+					<Gem img="gems/white-sapphire.png" name="White Sapphires" viewMoreUrl="/white+sapphires" />
+				</div>
 			</div>
 		</div>
 	);
