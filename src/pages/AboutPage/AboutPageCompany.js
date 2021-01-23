f7b07a37957ce6899601543a888cdaa9fa2757ab
Nazhim <nazhimkalam@gmail.com>
2021-01-23 21:18:47 +0530

    completed the company page for the about page

--- src/pages/AboutPage/AboutPageCompany.js
+++ src/pages/AboutPage/AboutPageCompany.js
@@ -22,8 +22,24 @@ function AboutPageCompany() {
 					<img src="purple-sapphire.png" alt="" />
 				</div>
 			</div>
-			<div className="aboutPageCompanyMiddle"></div>
-			<div className="aboutPageCompanyLast"></div>
+			<div className="aboutPageCompanyMiddle">
+				<p className="aboutPageCompanyMiddleFirst"></p>
+				<p className="aboutPageCompanyMiddleMiddle"></p>
+				<p className="aboutPageCompanyMiddleLast"></p>
+			</div>
+			<div className="aboutPageCompanyLast">
+				<h2>2020</h2>
+				<h2>THE JOURNEY</h2>
+				<p>
+					An icon for a new era, Amirah Gems debuts. It captures the courage, strength and optimism that Amirah stands
+					for.
+				</p>
+				<p>
+					Our founder, Suhail Mahmud is a Sri Lankan entrepreneur and designer that founded the company that builds
+					exceptional jewelry
+				</p>
+				<p>Amirah Gems starts manufacturing jewelry designs, making luxury available to Sri Lankans and the world.</p>
+			</div>
 		</div>
 	);
 }
