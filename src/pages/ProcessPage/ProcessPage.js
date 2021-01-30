53cbc608dde8620538cea98ca3f70f6ead257031
Nazhim <nazhimkalam@gmail.com>
2021-01-30 15:22:25 +0530

    completed the process page

--- src/pages/ProcessPage/ProcessPage.js
+++ src/pages/ProcessPage/ProcessPage.js
@@ -1,7 +1,6 @@
 import './ProcessPage.css';
 import ProcessStep from './ProcessStep/ProcessStep';
 
-
 function ProcessPage() {
 	return (
 		<div className="processPage">
@@ -31,11 +30,36 @@ function ProcessPage() {
 					reflect={false}
 				/>
 				<ProcessStep
-					img="/process/sketch.png"
-					stepNo="1"
-					stepName="The Sketch"
-					description="Sketches are created to illustrate ideas which helps to express scale, shape and design options. Sketches develop an overall direction, and as setting styles are considered, shapes and forms are explored and a design emerges"
+					img="/process/ring.png"
+					stepNo="2"
+					stepName="The Model"
+					description="Once the jewelry design is approved, it is being
+					translated into a three dimensional model using
+					computer aided design (CAD). This enables the
+					customer to view the jewelry three
+					dimensionally before it is cast."
+					reflect={true}
+				/>
+				<ProcessStep
+					img="/process/factory.png"
+					stepNo="3"
+					stepName="Workshop"
+					description="Craftsmen use traditional skills and techniques
+					to complete any details of the jewelry design,
+					including engraving and setting gemstones."
 					reflect={false}
+				/>
+				<ProcessStep
+					img="/process/polishing.png"
+					stepNo="4"
+					stepName="Final Touches"
+					description="Piece of jewelry is finished and it only needs a
+					light final polish to bring out the luster and
+					shine of the gold. Usually done using a soft
+					cotton cloth and buffing the piece by hand,
+					making sure that all the details and highlights
+					shine."
+					reflect={true}
 				/>
 			</div>
 		</div>
