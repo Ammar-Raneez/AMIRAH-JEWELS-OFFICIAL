f36f3ccda2ddd39dd3923222d179884762d614c0
Ammar <ammarraneez@gmail.com>
2021-01-31 11:58:58 +0530

    moved common code of about pages to dedicated component

--- src/pages/AboutPage/AboutPagePolicy.js
+++ src/pages/AboutPage/AboutPagePolicy.js
@@ -1,47 +1,17 @@
 import React from 'react'
-import 'AboutPage.css';
+import About from './About/About';
 
 function AboutPagePolicy() {
 	return (
-		<div className="aboutPageCompany">
-			<div className="aboutPageCompanyTop">
-				{/* jewel image */}
-				<div className="aboutPageCompanyTop__firstJewel">
-					<img src="purple-sapphire.png" alt="" />
-				</div>
-
-				<div className="aboutPageCompanyTop__middleContent">
-					<h2>AMIRAH GEMS POLICY</h2>
-					<p>
-						Every story has a beginning. Discover how Amirah Gems began in 2020 and grew into a global design house at
-						the forefront of innovative jewelry design and expert craftsmanship
-					</p>
-				</div>
-
-				{/* jewel image */}
-				<div className="aboutPageCompanyTop__lastJewel">
-					<img src="aboutuspage/purple-sapphire.png" alt="" />
-				</div>
-			</div>
-			<div className="aboutPageCompanyMiddle">
-				<p className="aboutPageCompanyMiddleFirst"></p>
-				<p className="aboutPageCompanyMiddleMiddle"></p>
-				<p className="aboutPageCompanyMiddleLast"></p>
-			</div>
-			<div className="aboutPageCompanyLast">
-				<h2>2020</h2>
-				<h2>THE JOURNEY</h2>
-				<p>
-					An icon for a new era, Amirah Gems debuts. It captures the courage, strength and optimism that Amirah stands
-					for.
-				</p>
-				<p>
-					Our founder, Suhail Mahmud is a Sri Lankan entrepreneur and designer that founded the company that builds
-					exceptional jewelry
-				</p>
-				<p>Amirah Gems starts manufacturing jewelry designs, making luxury available to Sri Lankans and the world.</p>
-			</div>
-		</div>
+		<About 
+			title="AMIRAH GEMS POLICY"
+			firstPara="Every story has a beginning. Discover how Amirah Gems began in 2020 and grew into a global design house at the forefront of innovative jewelry design and expert craftsmanship"
+			secondHeader="2020"
+			secondSubHeader="THE JOURNEY"
+			secondFirstPara="An icon for a new era, Amirah Gems debuts. It captures the courage, strength and optimism that Amirah stands for."
+			secondSecondPara="Our founder, Suhail Mahmud is a Sri Lankan entrepreneur and designer that founded the company that builds exceptional jewelry"
+			secondThirdPara="Amirah Gems starts manufacturing jewelry designs, making luxury available to Sri Lankans and the world."
+		/>
 	);
 }
 
