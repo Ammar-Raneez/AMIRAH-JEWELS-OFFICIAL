5cacde17b78cc5e5e835849e69e0c47de415f3a4
Nazhim <nazhimkalam@gmail.com>
2021-01-31 16:18:54 +0530

    commit

--- src/pages/GemsPages/SapphireViewDetails/BlueGemDetail.js
+++ src/pages/GemsPages/SapphireViewDetails/BlueGemDetail.js
@@ -1,19 +1,86 @@
-import './GemDetails.css'
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './GemDetails.css';
 
 function BlueGemDetail() {
-    return (
-        <div className="gemDetails">
-            
-            {/* Blue gem add to cart section */}
+	const [displayImage, setDisplayImage] = useState('gems/teal-sapphire.png');
+	const selectedImage = (imagePath) => {
+		console.log('/' + imagePath.split('/').reverse()[1] + '/' + imagePath.split('/').reverse()[0]);
+		setDisplayImage('/' + imagePath.split('/').reverse()[1] + '/' + imagePath.split('/').reverse()[0]);
+	};
 
-            {/* video content */}
+	return (
+		<div className="gemDetails">
+			{/* Blue gem add to cart section */}
+			<div className="gemDetails__sectionCart">
+				<div className="gemDetails__sectionCartSmallImages">
+					<img
+						onClick={(e) => {
+							selectedImage(e.target.src);
+						}}
+						src="gems/purple-sapphire.png"
+						alt=""
+					/>
+					<img
+						onClick={(e) => {
+							selectedImage(e.target.src);
+						}}
+						src="gems/teal-sapphire.png"
+						alt=""
+					/>
+					<img
+						onClick={(e) => {
+							selectedImage(e.target.src);
+						}}
+						src="gems/teal-sapphire.png"
+						alt=""
+					/>
+				</div>
+				<div className="gemDetails__sectionCartMainImage">
+					<img src={displayImage} alt="" />
+					<div className="gemDetails__sectionCartMainImageIcon">
+						<FavoriteBorderIcon />
+					</div>
+				</div>
+				<div className="gemDetails__sectionCartCartDetails">
+					<h2>Deep Royal Blue Sapphire</h2>
+					<div className="gemDetails__sectionCartCartDetailsItem">
+						<p>Quantity</p>
+						<p>1</p>
+					</div>
+					<div className="gemDetails__sectionCartCartDetailsItem">
+						<p>Weight (Carat)</p>
+						<p>0.3</p>
+					</div>
+					<div className="gemDetails__sectionCartCartDetailsItem">
+						<p>Shape</p>
+						<p>Round</p>
+					</div>
+					<p>Price: $ 1100.00</p>
+					<br />
+					<br />
+					<div className="gemDetails__sectionCartCartDetailsBtns">
+						<Link>
+							<button>ADD TO CART</button>
+						</Link>
+						<Link>
+							<button>VIEW DETAILS</button>
+						</Link>
+					</div>
+				</div>
+			</div>
 
-            {/* description and details */}
+			{/* video content */}
+			<div className="gemDetails__sectionVideo"></div>
 
-            {/* other similar products */}
+			{/* description and details */}
+			<div className="gemDetails__sectionDescription"></div>
 
-        </div>
-    )
+			{/* other similar products */}
+			<div className="gemDetails__sectionProduct"></div>
+		</div>
+	);
 }
 
-export default BlueGemDetail
+export default BlueGemDetail;
