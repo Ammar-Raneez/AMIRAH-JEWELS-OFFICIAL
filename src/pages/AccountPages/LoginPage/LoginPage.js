44aa48759397191938fac43b7f0ca10ee6abdd5e
Ammar <ammarraneez@gmail.com>
2021-01-31 12:48:17 +0530

    login page progressing

--- src/pages/AccountPages/LoginPage/LoginPage.js
+++ src/pages/AccountPages/LoginPage/LoginPage.js
@@ -5,10 +5,16 @@ function LoginPage() {
     return (
         <div className="loginPage">
             <div className="loginPage__leftSide">
-
+                <p className="loginPage__title">SIGN IN</p>
+                <p className="loginPage__subTitle">Please sign in to your Amirah account</p>
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <button>SIGN IN</button>
             </div>
             <div className="loginPage__rightSide">
-                
+                <p className="loginPage__title">CREATE AN ACCOUNT</p>
+                <p className="loginPage__subTitle">Save time during checkout, view your shopping bag and saved items from any device and access your order history.</p>
+                <button>REGISTER NOW</button>
             </div>
         </div>
     )
