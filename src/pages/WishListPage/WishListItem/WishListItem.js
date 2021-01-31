1572aa58f6af099784fb6d33630c30da983a0985
Nazhim <nazhimkalam@gmail.com>
2021-01-31 13:25:13 +0530

    completed wishlist

--- src/pages/WishListPage/WishListItem/WishListItem.js
+++ src/pages/WishListPage/WishListItem/WishListItem.js
@@ -5,28 +5,32 @@ import LocalMallIcon from '@material-ui/icons/LocalMall';
 function WishListItem() {
 	return (
 		<div className="wishListItem">
-			<div className="wishListItem__sectionOne">
-				<CloseIcon />
-			</div>
-			<div className="wishListItem__sectionTwo">
-				<div className="wishListItem__img">
-					<img src="/wishListPage/ring.png" alt="" />
+			<div className="wishListItem__main">
+				<div className="wishListItem__sectionOne">
+					<CloseIcon />
 				</div>
+				<div className="wishListItem__sectionTwo">
+					<div className="wishListItem__img">
+						<img src="/wishListPage/ring.png" alt="" />
+					</div>
 
-				<div className="wishListItem__details">
-					<h2>Diamond and Black Onyx Circle Pendant</h2>
-					<div className="wishListItem__detailsCart">
-						<LocalMallIcon />
-						<p>Add to cart</p>
+					<div className="wishListItem__details">
+						<h2>Diamond and Black Onyx Circle Pendant</h2>
+						<div className="wishListItem__detailsCart">
+							<LocalMallIcon />
+							<p>Add to cart</p>
+						</div>
+						<div className="wishListItem__detailsQuantity">
+							<p>Quantity</p>
+							<p>1+</p>
+						</div>
+						<div className="wishListItem__price">
+							<p>Price: $890.00</p>
+						</div>
 					</div>
-					<div className="wishListItem__detailsQuantity">
-						<p>Quantity</p>
-						<p>1+</p>
-					</div>
-					<p>Price: $890.00</p>
 				</div>
 			</div>
-            <hr/>
+			<hr />
 		</div>
 	);
 }
