9e06c426fabffaa59d2294836fd4ca7f163de59f
Nazhim <nazhimkalam@gmail.com>
2021-02-01 23:11:41 +0530

    created the reducer

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { StateProvider } from './StateProvider';
+import { initialState } from './reducer';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+	<StateProvider initialState={initialState} reducer={}>
+		<App />
+	</StateProvider>,
+	document.getElementById('root')
 );
