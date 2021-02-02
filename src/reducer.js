cc5ee66795a945c04613383d94132e43e03d3c29
Nazhim <nazhimkalam@gmail.com>
2021-02-03 01:05:23 +0530

    commit

--- src/reducer.js
+++ src/reducer.js
@@ -1,9 +1,10 @@
 export const initialState = {
-	basket: [],
+	cartBasket: [],
+	wishListBasket: [],
 	user: null,
 };
 
-function reducer(state, action) {
+const reducer = (state, action) => {
 	switch (action.type) {
 		case 'SET_USER':
 			return {
@@ -12,13 +13,44 @@ function reducer(state, action) {
 			};
 		case 'ADD_TO_BASKET':
 			// Logic for adding item to basket
-			break;
+			return { state };
 		case 'REMOVE_FROM_BASKET':
 			// Logic for removing item to basket
-			break;
+			return { state };
+		case 'ADD_TO_WISHLIST':
+			// Logic for adding item to the wishlist
+			return {
+				...state,
+				wishListBasket: [...state.wishListBasket, action.item],
+			};
+		case 'REMOVE_FROM_WISHLIST':
+			// Logic for removing item to the wishlist
+
+			// we cloned the basket
+			let newWishListBasket = [...state.wishListBasket];
+
+			// finding the index of the item we are looking for to be deleted from the list
+			const index = state.wishListBasket.findIndex((wishListItem) => wishListItem.name === action.name);
+
+			if (index >= 0) {
+				// item exists in wishlist basket, remove it (removes the item from the basket for the index)
+				newWishListBasket.splice(index, 1);
+			} else {
+				console.warn(`Cant remove product id ${action.id} as its not in the wishlist basket`);
+			}
+
+			// return the updated basket back
+			return { ...state, wishListBasket: newWishListBasket };
 		default:
 			return state;
 	}
-}
+};
 
 export default reducer;
+
+// Details of the wishlist for each item
+// itemImage
+// cost of item
+// name of the item
+
+// Details of the cart for each item
