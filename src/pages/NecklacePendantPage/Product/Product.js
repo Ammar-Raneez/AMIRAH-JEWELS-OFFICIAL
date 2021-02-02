b9539640ae5e3842549efcfb48ab61b622ff2383
Ammar <ammarraneez@gmail.com>
2021-02-02 23:26:14 +0530

    gift page made responsive for smaller screens

--- src/pages/NecklacePendantPage/Product/Product.js
+++ src/pages/NecklacePendantPage/Product/Product.js
@@ -3,9 +3,11 @@ import './Product.css'
 
 function Product({ img, name, viewMoreUrl }) {
     const history = useHistory();
+
+    const extraClassName = name.indexOf(" ") > 0 ? "smallerFont" : "";
     
     return (
-        <div className="product">
+        <div className={"product " + extraClassName}>
             <img src={img} alt="" />
 			<p>{name}</p>
 			<button onClick={() => history.push(viewMoreUrl)}>VIEW MORE</button>
