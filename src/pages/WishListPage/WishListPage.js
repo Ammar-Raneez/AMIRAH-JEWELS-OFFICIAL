6875d68f8da5c5436b2f0ce5f55814d544923519
Nazhim <nazhimkalam@gmail.com>
2021-02-04 22:28:38 +0530

    commit

--- src/pages/WishListPage/WishListPage.js
+++ src/pages/WishListPage/WishListPage.js
@@ -15,9 +15,12 @@ function WishListPage() {
 	useEffect(() => {
 		// user logged in only we load the details for the particular user
 		// setLoading(true)
+
 		db.collection('users').onSnapshot((snapshot) =>
 			snapshot.docs.forEach((doc) => {
+				console.log("fetching all the data from the database")
 				if (doc.id === user?.email) {
+					console.log("OUTPUTTT");
 					// adding the cart items
 					for (const cartItem of doc.data().cart) {
 						console.log('Adding items from the database into the cart');
@@ -72,21 +75,24 @@ function WishListPage() {
 					<img src="/loading/loading.gif" alt="" width="200" />
 				</div>
 			) : (
-				<div 
-					className="wishListPage__items" 
-					style={ wishListBasket.length < 4 && wishListBasket.length > 0 ? { justifyContent: 'flex-start' } : { justifyContent: 'space-around' } }
-				>
-					{
-						wishListBasket.length == 0 ? (
-							<div className="wishListPage__itemsEmpty">
-								<h1>Your Wish List is Empty</h1>
-								<p>Add Items To Your Wishlist to Display them Here</p>
-							</div>
-						) : (
-							wishListBasket.map((item, x) => (
-							<WishListItem key={x} img={item.imgURL} title={item.name} currency="$" price={item.cost} />
-						)))
+				<div
+					className="wishListPage__items"
+					style={
+						wishListBasket.length < 4 && wishListBasket.length > 0
+							? { justifyContent: 'flex-start' }
+							: { justifyContent: 'space-around' }
 					}
+				>
+					{wishListBasket.length == 0 ? (
+						<div className="wishListPage__itemsEmpty">
+							<h1>Your Wish List is Empty</h1>
+							<p>Add Items To Your Wishlist to Display them Here</p>
+						</div>
+					) : (
+						wishListBasket.map((item, x) => (
+							<WishListItem key={x} img={item.imgURL} title={item.name} currency="$" price={item.cost} />
+						))
+					)}
 				</div>
 			)}
 		</div>
@@ -94,10 +100,12 @@ function WishListPage() {
 		<div className="wishListPage">
 			<div className="wishListPage__itemsNoLogin">
 				<h1>Login to be able to save items you would like to purchase later</h1>
-				<a href="/login"><button>Login</button></a>
+				<a href="/login">
+					<button>Login</button>
+				</a>
 			</div>
 		</div>
-	)
+	);
 }
 
 export default WishListPage;
