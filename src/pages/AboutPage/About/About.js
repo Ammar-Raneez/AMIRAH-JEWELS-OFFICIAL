b336dd7f3ad53cd6cecca39da898dee4c966fd5a
Ammar <ammarraneez@gmail.com>
2021-02-06 01:36:10 +0530

    about pages animated

--- src/pages/AboutPage/About/About.js
+++ src/pages/AboutPage/About/About.js
@@ -1,37 +1,44 @@
 import React from 'react'
 import './AboutPage.css'
+import { Fade } from 'react-awesome-reveal';
 
 function About({ title, firstPara, secondHeader, secondSubHeader, secondFirstPara, secondSecondPara, secondThirdPara }) {
     return (
 		<div className="aboutPageCompany">
 			<div className="aboutPageCompanyTop">
-				{/* jewel image */}
-				<div className="aboutPageCompanyTop__firstJewel">
-					<img src="aboutuspage/purple-sapphire.png" alt="" />
-				</div>
+				<Fade direction="left">
+					{/* jewel image */}
+					<div className="aboutPageCompanyTop__firstJewel">
+						<img src="aboutuspage/purple-sapphire.png" alt="" />
+					</div>
 
-				<div className="aboutPageCompanyTop__middleContent">
-					<h2>{title}</h2>
-					<p>{firstPara}</p>
-				</div>
+					<div className="aboutPageCompanyTop__middleContent">
+						<h2>{title}</h2>
+						<p>{firstPara}</p>
+					</div>
 
-				{/* jewel image */}
-				<div className="aboutPageCompanyTop__lastJewel">
-					<img src="aboutuspage/purple-sapphire.png" alt="" />
+					{/* jewel image */}
+					<div className="aboutPageCompanyTop__lastJewel">
+						<img src="aboutuspage/purple-sapphire.png" alt="" />
+					</div>
+				</Fade>
+			</div>
+			<Fade direction="down">
+				<div className="aboutPageCompanyMiddle">
+					<p className="aboutPageCompanyMiddleFirst"></p>
+					<p className="aboutPageCompanyMiddleMiddle"></p>
+					<p className="aboutPageCompanyMiddleLast"></p>
 				</div>
-			</div>
-			<div className="aboutPageCompanyMiddle">
-				<p className="aboutPageCompanyMiddleFirst"></p>
-				<p className="aboutPageCompanyMiddleMiddle"></p>
-				<p className="aboutPageCompanyMiddleLast"></p>
-			</div>
-			<div className="aboutPageCompanyLast">
-				<h2>{secondHeader}</h2>
-				<h2>{secondSubHeader}</h2>
-				<p>{secondFirstPara}</p>
-				<p>{secondSecondPara}</p>
-				<p>{secondThirdPara}</p>
-			</div>
+			</Fade>
+			<Fade direction="right">
+				<div className="aboutPageCompanyLast">
+					<h2>{secondHeader}</h2>
+					<h2>{secondSubHeader}</h2>
+					<p>{secondFirstPara}</p>
+					<p>{secondSecondPara}</p>
+					<p>{secondThirdPara}</p>
+				</div>
+			</Fade>
 		</div>
     )
 }
