7c9c5827bc181b66e3cb8e8bbddb80fff76563d8
Ammar <ammarraneez@gmail.com>
2021-02-05 21:42:00 +0530

    placed Gems inside Paper as well (same as products)

--- src/pages/GemsPages/Gem/Gem.js
+++ src/pages/GemsPages/Gem/Gem.js
@@ -1,14 +1,17 @@
+import { Paper } from '@material-ui/core';
 import './Gem.css';
 
 function Gem({ img, name, viewMoreUrl }) {
 	// const history = useHistory();
 
 	return (
-		<div className="gem">
-			<img src={img} alt="" />
-			<p>{name}</p>
-			<a href={viewMoreUrl}><button>VIEW MORE</button></a>
-		</div>
+		<Paper className="gem">
+			<a href={viewMoreUrl}>
+				<img src={img} alt="" />
+				<p>{name}</p>
+			</a>
+			{/* <a href={viewMoreUrl}><button>VIEW MORE</button></a> */}
+		</Paper>
 	);
 }
 
