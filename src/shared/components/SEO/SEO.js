d75fc8be4273212f58d4366500e4ded0a029485c
Ammar <ammarraneez@gmail.com>
2021-02-07 15:55:21 +0530

    removed redundant "Amirah Jewellers" off title

--- src/shared/components/SEO/SEO.js
+++ src/shared/components/SEO/SEO.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Helmet from 'react-helmet';
 
 function SEO({ title }) {
-    const titleText = title ? `${title} Amirah Jewellers` : 'Amirah Jewellers';
+    const titleText = title ? `${title}` : 'Amirah Jewellers';
 
     return (
         <Helmet>
