ff63155b2de6d0a5192b5a14f53de5c5e01e6c4a
Ammar <ammarraneez@gmail.com>
2021-02-09 14:54:17 +0530

    wishlist made responsive wishlist passed responsiveness stress test

--- src/pages/WishListPage/WishListItem/WishListItem.js
+++ src/pages/WishListPage/WishListItem/WishListItem.js
@@ -89,7 +89,9 @@ function WishListItem({ img, title, currency, price }) {
 							</p>
 							<p>Add to Cart</p>
 						</div>
-						<p>{title}</p>
+						<div className="wishListItem__title">
+							<p>{title}</p>
+						</div>
 					</div>
 				</div>
 			</div>
