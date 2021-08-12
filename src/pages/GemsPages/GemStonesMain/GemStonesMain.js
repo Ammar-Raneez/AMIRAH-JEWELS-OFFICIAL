71928a3ecfba1203743454c9bbba69e7337ef07f
Nazhim <nazhimkalam@gmail.com>
2021-08-12 19:28:17 +0530

    linked the gems

--- src/pages/GemsPages/GemStonesMain/GemStonesMain.js
+++ src/pages/GemsPages/GemStonesMain/GemStonesMain.js
@@ -1,57 +1,62 @@
-import Gem from '../Gem/Gem';
-import './GemStonesMain.css';
-import { Fade } from 'react-awesome-reveal';
-import SEO from '../../../shared/components/SEO/SEO';
+import Gem from "../Gem/Gem";
+import "./GemStonesMain.css";
+import { Fade } from "react-awesome-reveal";
+import SEO from "../../../shared/components/SEO/SEO";
+import gemData from "../../../gemData.json";
 
 function GemStonesMain() {
-	return (
-		<div className="gemStonesMain">
-			<SEO title="Explore All Gems & Stones" />
-			<div className="gemStonesMain__details">
-				<div className="gemStonesMain__detailsFirst">
-					<Fade triggerOnce direction="left">
-						<h1>HIGH QUALITY SAPPHIRE STONES</h1>
-						<p>
-							We are the best known for the provision of the Highest Quality Sri Lankan gemstones and for sourcing
-							specific high quality stones upon customers request.
-						</p>
-						<p>
-							We are the leading suppliers of the highest quality rough and cut-and-polished gem stones for both locally
-							and internationally
-						</p>
-						<div className="gemStonesMain__detailsFirstButtons">
-							<a href="#gems">
-								<button>VIEW PRODUCTS</button>
-							</a>
-							<a href="/contactus">
-								<button>CALL US</button>
-							</a>	
-						</div>
-					</Fade>
-				</div>
-				<div className="gemStonesMain__detailsSecond">
-					<Fade triggerOnce direction="right">
-						<img src="https://images-aka.kay.com/kay/gemstone/blue-gems/blue-stone-names.jpg" alt="" />
-					</Fade>
-				</div>
-			</div>
+  return (
+    <div className="gemStonesMain">
+      <SEO title="Explore All Gems & Stones" />
+      <div className="gemStonesMain__details">
+        <div className="gemStonesMain__detailsFirst">
+          <Fade triggerOnce direction="left">
+            <h1>HIGH QUALITY SAPPHIRE STONES</h1>
+            <p>
+              We are the best known for the provision of the Highest Quality Sri
+              Lankan gemstones and for sourcing specific high quality stones
+              upon customers request.
+            </p>
+            <p>
+              We are the leading suppliers of the highest quality rough and
+              cut-and-polished gem stones for both locally and internationally
+            </p>
+            <div className="gemStonesMain__detailsFirstButtons">
+              <a href="#gems">
+                <button>VIEW PRODUCTS</button>
+              </a>
+              <a href="/contactus">
+                <button>CALL US</button>
+              </a>
+            </div>
+          </Fade>
+        </div>
+        <div className="gemStonesMain__detailsSecond">
+          <Fade triggerOnce direction="right">
+            <img
+              src="https://images-aka.kay.com/kay/gemstone/blue-gems/blue-stone-names.jpg"
+              alt=""
+            />
+          </Fade>
+        </div>
+      </div>
 
-			<div id="gems" className="gemStonesMain__otherGems">
-				<Fade cascade>
-					<div className="gemStonesMain__otherGemsRow">
-						<Gem img="gems/teal-sapphire.png" name="Teal Sapphires" viewMoreUrl="/teal+sapphire" />
-						<Gem img="gems/purple-sapphire.png" name="Purple Sapphires" viewMoreUrl="/purple-sapphire" />
-						<Gem img="gems/Padparadscha-sapphire.png" name="Padparadscha Sapphires" viewMoreUrl="/padparadscha-sapphire" />
-						<Gem img="gems/orange-sapphire.png" name="Orange Sapphires" viewMoreUrl="/orange-sapphire" />
-					</div>
-					<div style={{ justifyContent: 'center' }} className="gemStonesMain__otherGemsRow">
-						<Gem img="gems/ClaudiaHamann_PinkSapphire_Ceylon_Cushion_Unheated.png" name="Pink Sapphires" viewMoreUrl="/pink-sapphire" />
-						<Gem img="gems/white-sapphire.png" name="White Sapphires" viewMoreUrl="/white+sapphires" />
-					</div>
-				</Fade>	
-			</div>
-		</div>
-	);
+      <div id="gems" className="gemStonesMain__otherGems">
+        <Fade cascade>
+          <div className="gemStonesMain__otherGemsRow">
+            {gemData.map(({ title, titleImage, id }, key) => (
+              <Gem
+                img={titleImage}
+                name={title}
+                viewMoreUrl={`/gems/${id}`}
+                key={key}
+              />
+            ))}
+          </div>
+        </Fade>
+      </div>
+    </div>
+  );
 }
 
 export default GemStonesMain;
